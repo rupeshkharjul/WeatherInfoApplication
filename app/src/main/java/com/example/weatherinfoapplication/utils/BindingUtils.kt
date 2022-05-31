9b3b483c6c4fdea25bab31b1dd53d9205bf2fc9a
KHARJRA <rupesh_a_kharjul@whirlpool.com>
2022-05-31 14:20:10 +0530

    Some changes

--- app/src/main/java/com/example/weatherinfoapplication/utils/BindingUtils.kt
+++ app/src/main/java/com/example/weatherinfoapplication/utils/BindingUtils.kt
@@ -1,29 +1,12 @@
 package com.example.weatherinfoapplication.utils
 
-import android.text.format.DateUtils
 import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import java.text.SimpleDateFormat
 import java.util.*
 
-public class BindingUtils {
-
-    @BindingAdapter("isVisible")
-    fun setIsVisible(view: View, isVisible: Boolean) {
-        if (isVisible) {
-            view.visibility = View.VISIBLE
-        } else {
-            view.visibility = View.GONE
-        }
-    }
-
-
-    public fun formatDateWithPattern(date: Long): String {
-        val fmt = SimpleDateFormat("dd-MM-yyyy")
-        val date = Date(date * 1000)
-        return fmt.format(date)
-    }
+class BindingUtils {
 
 
     companion object {
